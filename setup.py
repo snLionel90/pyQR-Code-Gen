d6f58deafe73f2db84114da7393e279fd30852a0
Lionel Sanchez <58988565+snLionel90@users.noreply.github.com>
2020-08-09 13:19:19 +0200

    Update setup.py
    O

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: TTS text to speech :: snLionel90 ",
+        "License :: pyqrcode :: snLionel90 ",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
